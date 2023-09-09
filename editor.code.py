f042e413d865160796b41f522da580a1a6362254
DaaTuNna123 <143889749+DaaTuNna123@users.noreply.github.com>
2023-09-09 18:50:08 +0400

    Add files via upload

---
+++ editor.code.py
@@ -0,0 +1,50 @@
+from PyQt5.QtWidgets import QApplication , QPushButton, QWidget,  QLabel, QHBoxLayout, QVBoxLayout, QListWidget
+
+
+app = QApplication([])
+
+window = QWidget()
+window.resize(700, 400)
+
+window.setWindowTitle("Easy Editor App")
+
+
+button_left = QPushButton('Left')
+button_Right = QPushButton('Right')
+button_Mirror = QPushButton('Mirror')
+button_Sharpness = QPushButton('Sharpness')
+button_BW = QPushButton('BW')
+
+Button_Folder = QPushButton('Folder')
+
+Label_pic = QLabel("")
+
+List_Widget = QListWidget()
+#layout_თან მუშაობა
+button_row = QHBoxLayout()
+
+button_row.addWidget(button_left)
+button_row.addWidget(button_Right)
+button_row.addWidget(button_Mirror)
+button_row.addWidget(button_Sharpness)
+button_row.addWidget(button_BW)
+
+
+row = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+
+col1.addWidget(Button_Folder)
+col1.addWidget(List_Widget)
+
+col2.addWidget(Label_pic)
+col2.addLayout(button_row)
+
+row.addLayout(col1)
+row.addLayout(col2)
+
+
+
+window.setLayout(row)
+window.show()
+app.exec()
