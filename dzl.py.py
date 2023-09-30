7ad30dc000fb1e9bf8c59e3cd22dda98d11a937a
DaaTuNna123 <143889749+DaaTuNna123@users.noreply.github.com>
2023-09-30 19:49:59 +0400

    Add files via upload

--- dzl.py.py
+++ dzl.py.py
@@ -1,4 +1,7 @@
 from PyQt5.QtWidgets import QApplication , QPushButton, QWidget,  QLabel, QHBoxLayout, QVBoxLayout, QListWidget, QFileDialog
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PIL import Image
 import os
 
 app = QApplication([])
@@ -75,63 +78,66 @@ def showFilenamesList():
     for i in filtered_list:
         List_Widget.addItem(i)
            
-    
-    
+class ImageProcesor():
+    def __init__(self):
+        self.image=None #აქ იქნება ფოტოს ობიექტი
+        self.Filename=None #აქ იქნება ფოტოს სახელი
+        self.dir=None #აქ იქნება დირექტორიის მისამართი
+        self.save_dir="Modified/" #ფოლდერი სადაც შევინახავთ სახეშეცვლილ ფოტოებს
+
+    ''' LoadImage ფოტოს ჩატვირთვის ფუნქცია'''
+
+    def loadImage(self, filename):
+        '''When loading, remember the path and file name '''
+        self.filename=filename #გადავცემთ სახელს
+        fullname = os.path.join(workdir, filename) #სრული მისამართის ცვლადი
+        self.image = Image.open(fullname) #self.image აქ არის უკვე PIL ის ობიექტი
+
+    def SaveImage(self):
+        path=os.path.join(workdir, self.save_dir) #ვქმნით შესანახ მისამართს 
+        if not (os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path) #ვქმნით ფოლდერს თუ არ გვაქვს
+        fullname = os.path.join(path, self.filename)
+        self.image.save(fullname)
+
+        '''showImage გვიჩვენებს ფოტოს ეკრანზე'''
+
+    def showImage(self,path):
+        Label_pic.hide() #labelის დაფარვა
+        pixmap = QPixmap(path) #Pixmap ის ობიექტი
+        w,h = Label_pic.width(),Label_pic.height()
+        piximage = pixmap.scaled(w, h, Qt.KeepAspectRatio)
+        Label_pic.setPixmap(piximage)
+        Label_pic.show()
+
+
+    def do_bw(self):
+        self.image = self.image.convert('L')
+        obj = os.path.join(workdir, self.save_dir, self.filename)
+        self.SaveImage()
+        self.showImage(obj)
+        
+
+
+def showChoosenImage():
+    if List_Widget.currentRow() >=0:
+        print(List_Widget.currentRow())
+        filename = List_Widget.currentItem().text()
+        obj.loadImage(filename)
+        obj.showImage(os.path.join(workdir, filename))
+
+
+
+
+
+
+
 
 Button_Folder.clicked.connect(showFilenamesList)
-
+obj = ImageProcesor()
+List_Widget.currentRowChanged.connect(showChoosenImage)
+button_BW.clicked.connect(obj.do_bw)
 app.exec()
 
-
-# from PyQt5.QtWidgets import QApplication , QPushButton, QWidget,  QLabel, QHBoxLayout, QVBoxLayout, QListWidget
-
-
-# app = QApplication([])
-
-# window = QWidget()
-# window.resize(700, 400)
-
-# window.setWindowTitle("Easy Editor App")
-
-
-# button_left = QPushButton('Left')
-# button_Right = QPushButton('Right')
-# button_Mirror = QPushButton('Mirror')
-# button_Sharpness = QPushButton('Sharpness')
-# button_BW = QPushButton('BW')
-
-# Button_Folder = QPushButton('Folder')
-
-# Label_pic = QLabel("")
-
-# List_Widget = QListWidget()
-# #layout_თან მუშაობა
-# button_row = QHBoxLayout()
-
-# button_row.addWidget(button_BW)
-# button_row.addWidget(button_Sharpness)
-# button_row.addWidget(button_Mirror)
-# button_row.addWidget(button_Right)
-# button_row.addWidget(button_left)
-
-
-# row = QHBoxLayout()
-# col1 = QVBoxLayout()
-# col2 = QVBoxLayout()
-
-# col2.addWidget(Button_Folder)
-# col2.addWidget(List_Widget)
-
-# col1.addWidget(Label_pic)
-# col1.addLayout(button_row)
-
-# row.addLayout(col1)
-# row.addLayout(col2)
-
-
-
-# window.setLayout(row)
-# window.show()
-# app.exec()
 
 
